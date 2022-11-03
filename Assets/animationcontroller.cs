bc5ebe55941366a7257e24c57221664074d1ba81
Yash Nishikant <yash0312@hotmail.com>
2022-11-02 20:39:15 -0400

    AnimationDemos

---
+++ Assets/animationcontroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationcontroller : MonoBehaviour
+{
+
+    [SerializeField] private GameObject cube;
+    [SerializeField] private GameObject sphere;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            cube.GetComponent<Animation>().Play();
+
+            sphere.GetComponent<Animation>().Play();
+        }
+    }
+}
